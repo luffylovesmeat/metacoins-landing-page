90466fd62ebebbceadb6438aeda4be4610080929
Uddeshya Tiwari <uddeshyatiwari9085@gmail.com>
2022-02-08 19:19:14 +0530

    fixing the roadmap section on home page

--- src/components/roadmap/Roadmap.jsx
+++ src/components/roadmap/Roadmap.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./roadmap.scss";
 import roadmapImg from "../../assets/images/roadmap.svg";
+import roadmapVImg from "../../assets/images/roadmap-v.svg";
 
 const Roadmap = () => {
+  const [state, setState] = useState(window.innerWidth <= 768 ? false : true);
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setState(window.innerWidth <= 768 ? false : true);
+    });
+  });
   return (
     <div className="roadmap">
       <div className="align-right">
@@ -19,22 +26,26 @@ const Roadmap = () => {
       <div className="roadmap-map">
         <div className="roadmaps">
           <div className="map map-1">
+            <h3>Q1, 2022</h3>
             Website Launch Pre-hype Raise Seed Funding Development Plan
           </div>
           <div className="map map-2">
+            <h3>Q2, 2022</h3>
             Community Building Token Generation NFT Marketplace Launch DEX
             Launch (Muliple AMMs) Onboard Gampay NFTs
           </div>
           <div className="map map-3">
+            <h3>Q3, 2022</h3>
             Launch Token Vesting Service Round 1 Fund Raise Launchpad Release
             Onboard Gampay IDO In Launchpad Onboarding to DEXes
           </div>
           <div className="map map-4">
+            <h3>Q4, 2022</h3>
             Launch Token Vesting Service Round 2 Fund Raise Launchpad Release
             Onboard Gampay IDO In Launchpad Onboarding to DEXes
           </div>
         </div>
-        <img src={roadmapImg} alt={roadmapImg} />
+        <img src={state ? roadmapImg : roadmapVImg} alt={roadmapImg} />
       </div>
     </div>
   );
